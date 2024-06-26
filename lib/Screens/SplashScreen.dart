3a7bd30e90f71257b6d787936a73e849334a79c8
anbumozhi-codoid-dev <anbumozhi.r@codoid.com>
2024-06-26 16:26:06 +0530

    changed in splashscreen

--- lib/Screens/SplashScreen.dart
+++ lib/Screens/SplashScreen.dart
@@ -22,9 +22,7 @@ class _SplashScreenState extends State<SplashScreen> {
     await Future.delayed(Duration(seconds: 3)); // Duration of the splash screen
     Navigator.of(context).pushReplacement(
       CustomPageRoute(
-        child: CustomCalendar(),
-        // child: MenstrualCalendarPage(),
-        // child: WelcomePage(),
+        child: WelcomePage(),
         begin: Offset(1.0, 0.0),
         end: Offset.zero,
         duration: 1,
@@ -86,95 +84,3 @@ class CustomPageRoute extends PageRouteBuilder {
   );
 }
 
-//
-// import 'dart:async';
-//
-// import 'package:vcount/Screens/DashBoard.dart';
-//
-// import '../../Utils/AllImports.dart';
-//
-// class SplashScreen extends StatefulWidget {
-//   const SplashScreen({Key? key}) : super(key: key);
-//
-//   @override
-//   State<SplashScreen> createState() => _SplashScreenState();
-// }
-//
-// class _SplashScreenState extends State<SplashScreen> {
-//   // String? gettoken;
-//   //
-//   // Future<String?> getToken() async{
-//   //   SharedPreferences prefs = await SharedPreferences.getInstance();
-//   //   String? token = prefs.getString('token'); //getting token from localstorage
-//   //   gettoken = token;
-//   //   return token;
-//   //
-//   // }
-//   @override
-//   void initState() {
-//     // TODO: implement initState
-//     super.initState();
-//     // getToken();
-//     // navigationPage();
-//     startTime();
-//
-//   }
-//   startTime() async {
-//     var duration = const Duration(seconds: 3);
-//     return Timer(duration, navigationPage);
-//   }
-//
-//   Future<void> navigationPage() async {
-//
-//     Navigator.push(
-//       context,
-//       MaterialPageRoute(
-//           builder: (context) =>  DashBoardScreen()),
-//     );
-//
-//   }
-//   @override
-//   Widget build(BuildContext context) {
-//     return   AnnotatedRegion(
-//       value: SystemUiOverlayStyle.light
-//           .copyWith(statusBarColor: AppTheme.primaryColor2),
-//       child: Scaffold(
-//
-//         body:
-//         Container(
-//           decoration:  BoxDecoration(
-//             gradient: LinearGradient(
-//               colors: [Colors.white, Colors.blue[100]!],
-//               begin: Alignment.topLeft,
-//               end: Alignment.bottomRight,
-//             ),
-//           ),
-//           child: Center(
-//               child: Padding(
-//                 padding:  EdgeInsets.all(20.0),
-//                 child: Column(
-//                   children: [
-//                     CommonUI().verticleAlign(height: 20.h),
-//                     // Image.asset(AppConstant.vcountLogo),
-//                     CommonUI().verticleAlign(height: 1.h),
-//                     Column(
-//                       mainAxisAlignment: MainAxisAlignment.start,
-//                       crossAxisAlignment: CrossAxisAlignment.end,
-//                       children: [
-//                         CommonUI().myText(text: "Welcome to VCOUNT",
-//
-//                             fontSize: 7.sp,color: AppTheme.black),
-//                         CommonUI().verticleAlign(height: 15.h)
-//
-//
-//                       ],
-//                     ),
-//                   ],
-//                 ),
-//               )
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
